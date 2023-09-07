ab7a1dcd822fa1a2417d51dca8e2c3e4303e520d
aliza zahid <alizayzah11@gmail.com>
2023-09-07 19:56:55 +0500

    add product.js

---
+++ addproduct.js
@@ -0,0 +1,3 @@
+const app = firebase.initializeApp(firebaseConfig);
+const storage = firebase.storage();
+const database = firebase.database(); // Reference to Firebase Realtime Database
---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyApMbd9Sncf9LWCqPLWS6Vj1Bm7beaOKF4",
+    authDomain: "smit-project-751b0.firebaseapp.com",
+    projectId: "smit-project-751b0",
+    storageBucket: "smit-project-751b0.appspot.com",
+    messagingSenderId: "824922240767",
+    appId: "1:824922240767:web:e6f9a800f7f2a10c3a3eca",
+    measurementId: "G-2T2Q9N891D"
+  };
